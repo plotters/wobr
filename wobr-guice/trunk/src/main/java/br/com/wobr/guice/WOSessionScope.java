d1994fab82831b9aa91a14b302cb6c1519e6aa4f
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-02-22 14:05:29 +0000

    Implementação do escopo WOSession.

--- wobr-guice/trunk/src/main/java/br/com/wobr/guice/WOSessionScope.java
+++ wobr-guice/trunk/src/main/java/br/com/wobr/guice/WOSessionScope.java
@@ -3,11 +3,60 @@ package br.com.wobr.guice;
 import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.Scope;
+import com.webobjects.appserver.WOSession;
+import com.webobjects.foundation.NSKeyValueCoding;
 
-public class WOSessionScope implements Scope
+import er.extensions.appserver.ERXSession;
+
+/**
+ * WOSession scope.
+ * 
+ * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ */
+class WOSessionScope implements Scope
 {
-	public <T> Provider<T> scope(Key<T> arg0, Provider<T> arg1)
+	/*
+	 * (non-Javadoc)
+	 * @see com.google.inject.Scope#scope(com.google.inject.Key,
+	 * com.google.inject.Provider)
+	 */
+	public <T> Provider<T> scope(final Key<T> key, final Provider<T> creator)
 	{
-		return null;
+		final String name = key.toString();
+
+		return new Provider<T>()
+		{
+			public T get()
+			{
+				WOSession session = session();
+
+				synchronized(session)
+				{
+					Object object = session.objectForKey(name);
+
+					if(object == NSKeyValueCoding.NullValue)
+					{
+						return null;
+					}
+
+					@SuppressWarnings("unchecked")
+					T t = (T) object;
+
+					if(t == null)
+					{
+						t = creator.get();
+
+						session.setObjectForKey(t != null ? t : NSKeyValueCoding.NullValue, name);
+					}
+
+					return t;
+				}
+			}
+		};
+	}
+
+	protected WOSession session()
+	{
+		return ERXSession.session();
 	}
 }
