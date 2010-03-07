a261ea3f72d105d52f4d795511340d429c57ded2
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-03-07 16:43:03 +0000

    [maven-release-plugin]  copy for tag wobr-guice-0.1

---
+++ wobr-parent/tags/wobr-guice-0.1/src/main/java/br/com/wobr/guice/WOSessionScoped.java
@@ -0,0 +1,22 @@
+package br.com.wobr.guice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.inject.ScopeAnnotation;
+
+/**
+ * Apply this to implementation classes when you want one instance per session.
+ * 
+ * @see com.google.inject.Scopes#SINGLETON
+ * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ * 
+ */
+@Target( { ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@ScopeAnnotation
+public @interface WOSessionScoped
+{
+}
