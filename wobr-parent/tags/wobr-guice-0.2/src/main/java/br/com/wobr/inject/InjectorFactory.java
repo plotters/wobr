25f94f4356707250294d05020c8faa871e12f7db
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-03-29 17:51:26 +0000

    [maven-release-plugin]  copy for tag wobr-guice-0.2

---
+++ wobr-parent/tags/wobr-guice-0.2/src/main/java/br/com/wobr/inject/InjectorFactory.java
@@ -0,0 +1,14 @@
+package br.com.wobr.inject;
+
+import com.google.inject.Injector;
+
+/**
+ * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ */
+public class InjectorFactory
+{
+	public static Injector create()
+	{
+		return InjectableApplication.application().injector();
+	}
+}
