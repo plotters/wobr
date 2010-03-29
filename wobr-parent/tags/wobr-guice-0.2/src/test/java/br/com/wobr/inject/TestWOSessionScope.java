a86c1313552bcd5637f04066beafbf187f58ba08
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-03-29 17:19:17 +0000

    [maven-release-plugin]  copy for tag wobr-guice-0.2

---
+++ wobr-parent/tags/wobr-guice-0.2/src/test/java/br/com/wobr/inject/TestWOSessionScope.java
@@ -0,0 +1,109 @@
+package br.com.wobr.inject;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.wobr.inject.WOSessionScope;
+
+import com.google.inject.Key;
+import com.google.inject.Provider;
+import com.webobjects.appserver.WOSession;
+import com.webobjects.foundation.NSKeyValueCoding;
+
+/**
+ * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestWOSessionScope
+{
+	private static final String KEY_NAME = "Key[type=java.lang.Object, annotation=[none]]";
+
+	@Mock
+	protected Provider<Object> mockCreator;
+
+	protected Key<Object> mockKey;
+
+	@Mock
+	protected Object mockObject;
+
+	@Mock
+	protected WOSession mockSession;
+
+	protected WOSessionScope scope;
+
+	@Test
+	public void createAndSaveObjectOnFirstCall() throws Exception
+	{
+		Provider<Object> result = scope.scope(mockKey, mockCreator);
+
+		assertThat(result.get(), is(mockObject));
+
+		Mockito.verify(mockCreator).get();
+		Mockito.verify(mockSession).setObjectForKey(mockObject, KEY_NAME);
+	}
+
+	@Test
+	public void retrieveTheSameObjectOnSubSequentCalls() throws Exception
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			Provider<Object> result = scope.scope(mockKey, mockCreator);
+
+			assertThat(result.get(), is(mockObject));
+		}
+
+		Mockito.verify(mockCreator, Mockito.times(1)).get();
+		Mockito.verify(mockSession, Mockito.times(1)).setObjectForKey(mockObject, KEY_NAME);
+	}
+
+	@Test
+	public void returnNullOnSubsequentCalls() throws Exception
+	{
+		Mockito.when(mockSession.objectForKey(KEY_NAME)).thenReturn(null, NSKeyValueCoding.NullValue);
+		Mockito.when(mockCreator.get()).thenReturn(null);
+
+		for(int i = 0; i < 10; i++)
+		{
+			Provider<Object> result = scope.scope(mockKey, mockCreator);
+
+			assertThat(result.get(), nullValue());
+		}
+
+		Mockito.verify(mockCreator, Mockito.times(1)).get();
+		Mockito.verify(mockSession, Mockito.times(1)).setObjectForKey(NSKeyValueCoding.NullValue, KEY_NAME);
+	}
+
+	@Test
+	public void setNullValueIfProvidesReturnsNull() throws Exception
+	{
+		Mockito.when(mockCreator.get()).thenReturn(null);
+
+		Provider<Object> result = scope.scope(mockKey, mockCreator);
+
+		assertThat(result.get(), nullValue());
+
+		Mockito.verify(mockSession).setObjectForKey(NSKeyValueCoding.NullValue, KEY_NAME);
+	}
+
+	@Before
+	public void setup()
+	{
+		scope = Mockito.spy(new WOSessionScope());
+
+		Mockito.doReturn(mockSession).when(scope).session();
+
+		mockKey = Key.get(Object.class);
+
+		Mockito.when(mockSession.objectForKey(KEY_NAME)).thenReturn(null, mockObject);
+
+		Mockito.when(mockCreator.get()).thenReturn(mockObject);
+	}
+}
