2ed10107c58ee96969bf06a7942f1dd13dd15edf
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-04-07 02:48:35 +0000

    Fixing test failure.

--- wobr-unit-test/trunk/src/main/java/br/com/wobr/unittest/matchers/SaveChangesMatcher.java
+++ wobr-unit-test/trunk/src/main/java/br/com/wobr/unittest/matchers/SaveChangesMatcher.java
@@ -11,9 +11,7 @@ import com.webobjects.eocontrol.EOEditingContext;
 
 /**
  * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
- * 
- * @param <T>
- *            a kind of <code>EOEditingContext</code>.
+ * @param <T> a kind of <code>EOEditingContext</code>.
  */
 public class SaveChangesMatcher<T extends EOEditingContext> extends TypeSafeMatcher<T>
 {
@@ -26,62 +24,64 @@ public class SaveChangesMatcher<T extends EOEditingContext> extends TypeSafeMatc
 		this.message = null;
 	}
 
-	public SaveChangesMatcher(final String message)
+	public SaveChangesMatcher( final String message )
 	{
 		this.message = message;
 	}
 
-	public void describeTo(final Description description)
+	public void describeTo( final Description description )
 	{
-		if(message != null)
+		if( message != null )
 		{
-			description.appendText("expecting exception other than \"");
-			description.appendText(message);
-			description.appendText("\" while saving the editing context\n     but got: ");
+			description.appendText( "expecting exception other than \"" );
+			description.appendText( message );
+			description.appendText( "\" while saving the editing context\n     but got: " );
 
-			if(exception == null)
+			if( exception == null )
 			{
-				description.appendText("no exception and the editing context was successfully saved");
+				description.appendText( "no exception and the editing context was successfully saved" );
 
 				return;
 			}
 
-			description.appendText(exception.getClass().getName());
-			description.appendText(": \"");
-			description.appendText(exception.getMessage());
-			description.appendText("\"");
+			description.appendText( exception.getClass().getName() );
+			description.appendText( ": \"" );
+			description.appendText( exception.getMessage() );
+			description.appendText( "\"" );
 
 			return;
 		}
 
-		if(exception != null)
+		if( exception != null )
 		{
-			description.appendText("successfully saved editing context\n     but got: ");
-			description.appendText(exception.getClass().getName());
-			description.appendText(": \"");
-			description.appendText(exception.getMessage());
-			description.appendText("\"");
+			description.appendText( "successfully saved editing context\n     but got: " );
+			description.appendText( exception.getClass().getName() );
+			description.appendText( ": \"" );
+			description.appendText( exception.getMessage() );
+			description.appendText( "\"" );
 
 			return;
 		}
+
+		description.appendText( "successfully saved editing context\n     but got: a successfully saved editing context\n" );
 	}
 
 	@Override
-	public boolean matchesSafely(final T editingContext)
+	public boolean matchesSafely( final T editingContext )
 	{
 		try
 		{
 			editingContext.saveChanges();
 		}
-		catch(Exception exception)
+		catch( Exception exception )
 		{
 			this.exception = exception;
 
-			if(message != null)
+			if( message != null )
 			{
-				Matcher<String> matcher = is(not(message));
+				Matcher<String> matcher = is( not( message ) );
 
-				return matcher.matches(exception.getMessage());
+				return matcher.matches( exception.getMessage() );
 			}
 
 			return false;
