9369f3272d3896efabb461caf01a3aaf15597907
hprange <hprange@ddf92454-1c72-11de-9433-ad2e3fad96c5>
2010-04-10 16:39:35 +0000

    Writing some javadocs.

--- wobr-unit-test/trunk/src/main/java/br/com/wobr/unittest/EOAssert.java
+++ wobr-unit-test/trunk/src/main/java/br/com/wobr/unittest/EOAssert.java
@@ -16,7 +16,37 @@ import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOEnterpriseObject;
 
 /**
+ * This class is the entry point for writing assertions to WebObjects related
+ * unit tests.
+ * <p>
+ * Before start using this framework, remember to use the import static feature
+ * in order to write more readable code:
+ * 
+ * <pre>
+ * import static br.com.wobr.unittest.EOAssert.*;
+ * </pre>
+ * 
+ * <h4>Confirm that an enterprise object can/cannot be saved</h4>
+ * 
+ * <pre>
+ * // Checks whether the eo can be saved
+ * confirm(eo, canBeSaved());
+ * </pre>
+ * 
+ * <pre>
+ * // Checks whether the eo cannot be saved
+ * confirm(eo, cannotBeSaved());
+ * </pre>
+ * 
+ * <pre>
+ * // Much safer check whether eo cannot be saved
+ * confirm(eo, cannotBeSavedBecause(&quot;The foo property cannot be null&quot;));
+ * </pre>
+ * 
+ * </pre>
+ * 
  * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ * @since 1.0
  */
 public class EOAssert
 {
@@ -108,5 +138,12 @@ public class EOAssert
 	public static Matcher<EOEditingContext> saveChanges()
 	{
 		return new SaveChangesMatcher<EOEditingContext>();
+	}
+
+	/**
+	 * <code>EOAssert</code> is not intended to be instantiated.
+	 */
+	private EOAssert()
+	{
 	}
 }
